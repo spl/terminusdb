86e81589b3b3ee83a52a586bcc20effa842134b7
Sean Leather <sean@terminusdb.com>
2021-10-15 13:17:14 +0200

    require(./params.js).Params

--- tests/lib/index.js
+++ tests/lib/index.js
@@ -2,6 +2,6 @@ exports.Agent = require('./agent.js').Agent
 exports.branch = require('./branch.js')
 exports.db = require('./db.js')
 exports.document = require('./document.js')
-exports.params = require('./params.js')
+exports.Params = require('./params.js').Params
 exports.remote = require('./remote.js')
 exports.util = require('./util.js')
